1660ea8170e6aa77d3c3f3b2b869f32333108d34
Mohammad Imad Ali <mrimadali@gmail.com>
2022-05-20 19:27:10 +0530

    Test package

--- Sources/TestPackage/UserTableViewCell.swift
+++ Sources/TestPackage/UserTableViewCell.swift
@@ -15,4 +15,13 @@ public class UserTableViewCell: UITableViewCell {
     public override func awakeFromNib() {
         super.awakeFromNib()
     }
+    
+    public static func loadFromNib() -> UserTableViewCell {
+        // Load the compiled XIB = NIB file from the module resources bundle
+        let bundle = Bundle.module
+        let nib = UINib(nibName: "UserTableViewCell", bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil)[0] as! UserTableViewCell
+        return view
+    }
+
 }
