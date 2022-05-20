d83c4479b7ab3484f848d0cfcf5ecf00f71fde91
Mohammad Imad Ali <mrimadali@gmail.com>
2022-05-20 18:18:31 +0530

    Changed accessibility.

--- Sources/TestPackage/UserTableViewCell.swift
+++ Sources/TestPackage/UserTableViewCell.swift
@@ -7,19 +7,14 @@
 
 import UIKit
 
-class UserTableViewCell: UITableViewCell {
+public class UserTableViewCell: UITableViewCell {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var subTitleLabel: UILabel!
-    override func awakeFromNib() {
+    
+    public override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
 }
