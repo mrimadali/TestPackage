a95adebd86706725e46449ada0063235e8373e9c
Mohammad Imad Ali <mrimadali@gmail.com>
2022-05-20 18:16:25 +0530

    Updated example app

--- Sources/TestPackage/UserTableViewCell.swift
+++ Sources/TestPackage/UserTableViewCell.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class UserTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subTitleLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
